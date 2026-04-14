f15dc6f6959a54ab57dc7c633afa1e7895de8135
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-04-14 11:10:04 +0700

    change

--- dependency-analyzer/test-project/index.js
+++
@@ -1,6 +0,0 @@
-import { add, multiply } from './utils/math';
-import { formatDate } from './utils/date';
-import { Button } from './components/Button';
-
-console.log(add(1, 2));
-console.log(formatDate(new Date()));
---
+++ test-project/index.js
@@ -0,0 +1,6 @@
+import { add, multiply } from './utils/math';
+import { formatDate } from '../dependency-analyzer/test-project/utils/date';
+import { Button } from '../dependency-analyzer/test-project/components/Button';
+
+console.log(add(1, 2));
+console.log(formatDate(new Date()));
